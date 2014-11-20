c5a013c45c7c248d3ec28d07920cbc84e5d4ad2b
Oladayo Osikoya <alwaysdeone@gmail.com>
2014-11-20 16:05:25 +0100

    Add facebook collections package. Fetch and display page posts and account object.

--- respondly-facebook.js
+++ respondly-facebook.js
@@ -1,11 +1,27 @@
 if (Meteor.isClient) {
-  Template.body.rendered = function() {
-    console.log('Me');
-  };
-}
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  Tracker.autorun(function(c) {
+    if (Meteor.user()) {
+      Me = FacebookCollections.getCollection("/me/accounts");
+      Posts = FacebookCollections.getCollection("/363420650498505/feed");
+    }
   });
+  
+  Accounts.ui.config({
+    requestPermissions: {
+      facebook: ['manage_pages', 'publish_actions'],
+    }
+  });
+
+  Template.screen.helpers({
+    posts: function() {
+      if (Meteor.user() && Posts)
+        return Posts.find().fetch();
+    },
+    account: function() {
+      if (Meteor.user() && Me)
+        return Me.find().fetch();
+    }
+  });
+
 }
